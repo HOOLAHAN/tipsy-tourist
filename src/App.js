0d6d019e26e255172671db46efcda99c4f6ded34
Kayleigh Watts <kwatts949@gmail.com>
2023-01-04 13:33:08 +0000

    Styling updates, add icon

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// Chakra styling
 import {
   Box,
   Button,
@@ -8,21 +9,25 @@ import {
   Input,
   Text,
 } from '@chakra-ui/react'
-import { FaLocationArrow, FaTimes } from 'react-icons/fa'
+import { FaLocationArrow, FaTimes, FaBeer } from 'react-icons/fa'
 
 function App() {
+
+  // styling
   return (
     <Flex
+    // background styling
       position='relative'
       flexDirection='column'
       alignItems='center'
-      bgColor='blue.200'
-      bgImage='https://images.unsplash.com/photo-1647117181799-0ac3e50a548a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
+      bgColor='teal.200'
+      bgImage="url('./public/images/cocktails.png')"
       bgPos='bottom'
       h='100vh'
       w='100vw'
     >
-      <Box position='absolute' left={0} top={0} h='100%' w='100%'></Box>
+      
+      <Box position='absolute' left={0} top={0} h='100%' w='100%'></Box> 
 
       <Box
         p={4}
@@ -34,27 +39,26 @@ function App() {
         zIndex='modal'
       >
         <HStack spacing={4}>
-          <Input type='text' placeholder='Origin' />
-          <Input type='text' placeholder='Destination' />
+          <Input type='text' placeholder='Start' />
+          <Input type='text' placeholder='Finish' />
           <ButtonGroup>
-            <Button colorScheme='pink' type='submit'>
-              Calculate Route
+            <Button leftIcon={<FaBeer />}colorScheme='green' type='submit'>
+              Plan my pub crawl!
             </Button>
             <IconButton
               aria-label='center back'
               icon={<FaTimes />}
-              onClick={() => alert(123)}
+              onClick={() => alert('Update this')}
             />
           </ButtonGroup>
         </HStack>
         <HStack spacing={4} mt={4} justifyContent='space-between'>
-          <Text>Distance: </Text>
-          <Text>Duration: </Text>
+          <Text>Total distance (walking): </Text>
           <IconButton
             aria-label='center back'
             icon={<FaLocationArrow />}
             isRound
-            onClick={() => alert(123)}
+            onClick={() => alert('Update this')}
           />
         </HStack>
       </Box>
