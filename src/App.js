21f96e2030e344e128a95f4a8f3de7fc27e1f2b2
Kayleigh Watts <kwatts949@gmail.com>
2023-01-04 15:10:26 +0000

    Update commit

--- src/App.js
+++ src/App.js
@@ -7,63 +7,87 @@ import {
   HStack,
   IconButton,
   Input,
+  SkeletonText,
   Text,
-} from '@chakra-ui/react'
-import { FaLocationArrow, FaTimes, FaBeer } from 'react-icons/fa'
+} from "@chakra-ui/react";
+import { FaLocationArrow, FaTimes, FaBeer } from "react-icons/fa"; // icons
+
+import { useJsApiLoader, GoogleMap } from "@react-google-maps/api"; // provides 'is loaded'
+
+const center = { lat: 51.5007, lng: 0.1246 };
 
 function App() {
+  // loads google maps script
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: "API_KEY", //process.env.GOOGLE_MAPS_API
+  });
+
+  // if script does not load, display SkeletonText
+  if (!isLoaded) {
+    return <SkeletonText />;
+  }
 
   // styling
   return (
     <Flex
-    // background styling
-      position='relative'
-      flexDirection='column'
-      alignItems='center'
-      bgColor='teal.200'
-      bgImage="url('./public/images/cocktails.png')"
-      bgPos='bottom'
-      h='100vh'
-      w='100vw'
+      // background styling
+      position="relative"
+      flexDirection="column"
+      alignItems="center"
+      bgColor="teal.200"
+      h="100vh"
+      w="100vw"
     >
-      
-      <Box position='absolute' left={0} top={0} h='100%' w='100%'></Box> 
+      <Box position="absolute" left={0} top={0} h="100%" w="100%">
+        {/* Google Map Box */}
+        <GoogleMap
+          center={center}
+          zoom={15}
+          mapContainerStyle={{ width: "100%", height: "100%" }}
+          options={{
+            zoomControl: false,
+            streetViewControl: false,
+            mapTypeControl: false,
+            fullScreenControl: false,
+          }}
+        ></GoogleMap>
+      </Box>
 
       <Box
         p={4}
-        borderRadius='lg'
+        borderRadius="lg"
         mt={4}
-        bgColor='white'
-        shadow='base'
-        minW='container.md'
-        zIndex='modal'
+        bgColor="white"
+        shadow="base"
+        minW="container.md"
+        zIndex="modal"
       >
         <HStack spacing={4}>
-          <Input type='text' placeholder='Start' />
-          <Input type='text' placeholder='Finish' />
+          <Input type="text" placeholder="Start" />
+          <Input type="text" placeholder="Finish" />
           <ButtonGroup>
-            <Button leftIcon={<FaBeer />}colorScheme='green' type='submit'>
-              Plan my pub crawl!
+            <Button leftIcon={<FaBeer />} colorScheme="green" type="submit">
+              Plan my Tipsy Tour!
             </Button>
             <IconButton
-              aria-label='center back'
+              aria-label="center back"
               icon={<FaTimes />}
-              onClick={() => alert('Update this')}
+              onClick={() => alert("Update this")}
             />
           </ButtonGroup>
         </HStack>
-        <HStack spacing={4} mt={4} justifyContent='space-between'>
+        <HStack spacing={4} mt={4} justifyContent="space-between">
           <Text>Total distance (walking): </Text>
           <IconButton
-            aria-label='center back'
+            aria-label="center back"
             icon={<FaLocationArrow />}
             isRound
-            onClick={() => alert('Update this')}
+            onClick={() => alert("Update this")}
           />
         </HStack>
       </Box>
     </Flex>
-  )
+  );
 }
 
-export default App
+export default App;
