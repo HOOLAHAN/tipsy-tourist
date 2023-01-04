52c991c99a5c5f373fb6647dfbbc8675f4b3c4e7
Kayleigh Watts <kwatts949@gmail.com>
2023-01-04 12:00:07 +0000

    Add chakra UI, update app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Flex,
+  HStack,
+  IconButton,
+  Input,
+  Text,
+} from '@chakra-ui/react'
+import { FaLocationArrow, FaTimes } from 'react-icons/fa'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Flex
+      position='relative'
+      flexDirection='column'
+      alignItems='center'
+      bgColor='blue.200'
+      bgImage='https://images.unsplash.com/photo-1647117181799-0ac3e50a548a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
+      bgPos='bottom'
+      h='100vh'
+      w='100vw'
+    >
+      <Box position='absolute' left={0} top={0} h='100%' w='100%'></Box>
+
+      <Box
+        p={4}
+        borderRadius='lg'
+        mt={4}
+        bgColor='white'
+        shadow='base'
+        minW='container.md'
+        zIndex='modal'
+      >
+        <HStack spacing={4}>
+          <Input type='text' placeholder='Origin' />
+          <Input type='text' placeholder='Destination' />
+          <ButtonGroup>
+            <Button colorScheme='pink' type='submit'>
+              Calculate Route
+            </Button>
+            <IconButton
+              aria-label='center back'
+              icon={<FaTimes />}
+              onClick={() => alert(123)}
+            />
+          </ButtonGroup>
+        </HStack>
+        <HStack spacing={4} mt={4} justifyContent='space-between'>
+          <Text>Distance: </Text>
+          <Text>Duration: </Text>
+          <IconButton
+            aria-label='center back'
+            icon={<FaLocationArrow />}
+            isRound
+            onClick={() => alert(123)}
+          />
+        </HStack>
+      </Box>
+    </Flex>
+  )
 }
 
-export default App;
+export default App
