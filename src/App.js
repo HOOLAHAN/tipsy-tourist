932b55c908e6dd0e60d7fc32d8e94c0f9484a435
Iain <iain.hoolahan@gmail.com>
2023-01-06 18:30:35 +0000

    resize logo image

--- src/App.js
+++ src/App.js
@@ -197,7 +197,7 @@ function App() {
         zIndex="1"
       >
         <HStack spacing={2} justifyContent="space-between">
-          <Image boxSize='70px' objectFit='cover' src={tipsyTouristLogo} alt="logo"/>
+          <Image boxSize='60px' objectFit='cover' src={tipsyTouristLogo} alt="logo"/>
           <Autocomplete>
             <Input type="text" placeholder="Start" ref={startRef} />
           </Autocomplete>
