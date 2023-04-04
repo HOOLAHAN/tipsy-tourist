896e72c994834296f67ff9baca38e29e7f043157
William Lines <williamlines42@gmail.com>
2023-04-04 17:01:11 +0100

    basic tooltip added

--- src/components/LocationDetailsCard.js
+++ src/components/LocationDetailsCard.js
@@ -15,22 +15,39 @@ import {
   Link,
   Text,
   Image,
-  Tooltip
-} from '@chakra-ui/react'
+  Tooltip,
+} from "@chakra-ui/react";
 
 import { StarIcon, LinkIcon, PhoneIcon, CalendarIcon } from "@chakra-ui/icons";
 
 import { useState, React } from "react";
 
-import {
-  FaHome,
-} from "react-icons/fa"; // icons
-
+import { FaHome } from "react-icons/fa"; // icons
 
 import tipsyTouristLogo3 from "../images/logo3.svg";
 
-const LocationDetailsCard = ({place_id}) => {
-  const { isOpen, onOpen, onClose } = useDisclosure()
+const OpenNow = ({ data }) => {
+  // if (data.opening_hours.open_now) {
+  //   console.log("location is currently open");
+  console.log("data from open now");
+  console.log(data);
+  if (data.opening_hours === undefined) return <Box></Box>;
+  else {
+    return (
+      <Tooltip
+        label={data.opening_hours.weekday_text[0]}
+        aria-label="A tooltip"
+      >
+        testing
+      </Tooltip>
+    );
+  }
+  //   } else {
+  //   return <Text>Closed</Text>;
+};
+
+const LocationDetailsCard = ({ place_id }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const [locationCardData, setLocationCardData] = useState({});
 
   async function getDetails(place_id) {
@@ -39,16 +56,16 @@ const LocationDetailsCard = ({place_id}) => {
       const place = await Details(place_id);
       const locationData = place.result;
       setLocationCardData(locationData);
-      console.log(locationData.opening_hours.open_now)
-      console.log(locationData)
+      console.log(locationData.opening_hours.open_now);
+      console.log(locationData);
 
       return locationData;
     }
   }
 
   async function handleClick() {
-    onOpen()
-    getDetails(place_id)
+    onOpen();
+    getDetails(place_id);
   }
 
   let imageLink = "";
@@ -56,21 +73,13 @@ const LocationDetailsCard = ({place_id}) => {
     imageLink = tipsyTouristLogo3;
   } else {
     imageLink = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=1000&photo_reference=${locationCardData.photos[0].photo_reference}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`;
-    console.log(imageLink)
+    console.log(imageLink);
   }
-
-  function openNow() {
-    if(locationCardData.opening_hours.open_now) {
-      console.log("location is currently open")
-      return "Open"
-    } else {
-      return "Closed"
-    }
-  }
- 
   return (
-      <Box>
-      <Button onClick={handleClick} bgColor="#38A169" color="white">More Info</Button>
+    <Box>
+      <Button onClick={handleClick} bgColor="#38A169" color="white">
+        More Info
+      </Button>
 
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
@@ -78,58 +87,50 @@ const LocationDetailsCard = ({place_id}) => {
           <ModalHeader as="b"> {locationCardData.name} </ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-          <Center>
-          <Box display="flex" mt="2" alignItems="center">
-            {Array(5)
-              .fill("")
-              .map((_, i) => (
-                <StarIcon
-                  key={i}
-                  color={
-                    i < Math.round(locationCardData.rating)
-                      ? "yellow.500"
-                      : "gray.300"
-                  }
-                />
-              ))}
-          </Box>
-        </Center>
-        <HStack>
-          <LinkIcon />
-        <Center>
-          <Link href={locationCardData.website}>
-            {locationCardData.name} - website
-          </Link>
-          </Center>
-        </HStack>
-        <HStack>
-          <PhoneIcon />
-          <Text>
-            {locationCardData.formatted_phone_number}
-          </Text>
-        </HStack>
-        <HStack>
-          <FaHome />
-          <Text>
-            {locationCardData.vicinity}
-          </Text>
-        </HStack>
-        <HStack>
-          <CalendarIcon />
-        <Tooltip label="Hey, I'm here!" aria-label='A tooltip'>
-          Test
-          { openNow()}
+            <Center>
+              <Box display="flex" mt="2" alignItems="center">
+                {Array(5)
+                  .fill("")
+                  .map((_, i) => (
+                    <StarIcon
+                      key={i}
+                      color={
+                        i < Math.round(locationCardData.rating)
+                          ? "yellow.500"
+                          : "gray.300"
+                      }
+                    />
+                  ))}
+              </Box>
+            </Center>
+            <HStack>
+              <LinkIcon />
+              <Center>
+                <Link href={locationCardData.website}>
+                  {locationCardData.name} - website
+                </Link>
+              </Center>
+            </HStack>
+            <HStack>
+              <PhoneIcon />
+              <Text>{locationCardData.formatted_phone_number}</Text>
+            </HStack>
+            <HStack>
+              <FaHome />
+              <Text>{locationCardData.vicinity}</Text>
+            </HStack>
+            <HStack>
+              <CalendarIcon />
+              <OpenNow data={locationCardData} />
+            </HStack>
 
-        </Tooltip>
-        </HStack>
-
-        <Center>
-          <Image src={imageLink} alt="no image" maxW="300px" />
-        </Center>
-        </ModalBody>
+            <Center>
+              <Image src={imageLink} alt="no image" maxW="300px" />
+            </Center>
+          </ModalBody>
         </ModalContent>
       </Modal>
-      </Box>
+    </Box>
   );
 };
 
