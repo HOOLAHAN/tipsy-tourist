3c1a3661bec04fd9a7aef686490055428ee20548
Iain <iain.hoolahan@gmail.com>
2024-03-06 17:59:10 +0000

    update index to react 18 syntax and packages update

--- src/index.js
+++ src/index.js
@@ -1,27 +1,32 @@
 import React from "react";
-import App from "./App";
-import { ChakraProvider } from "@chakra-ui/react";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { extendTheme } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import "@fontsource/raleway/400.css";
 import "@fontsource/open-sans/700.css";
+import App from "./App";
 
-import { createRoot } from "react-dom/client";
-
-const theme2 = extendTheme({
+// Define your theme configuration
+const theme = extendTheme({
   fonts: {
     heading: `'Raleway', sans-serif`,
     body: `'Raleway', sans-serif`,
   },
 });
 
-const root = createRoot(document.getElementById("root"));
+// Get the container element
+const container = document.getElementById("root");
+
+// Create a root
+const root = createRoot(container);
+
+// Render the app with BrowserRouter and ChakraProvider using the defined theme
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <ChakraProvider theme={theme2}>
+  <React.StrictMode>
+    <BrowserRouter>
+      <ChakraProvider theme={theme}>
         <App />
       </ChakraProvider>
-    </React.StrictMode>
-  </BrowserRouter>
+    </BrowserRouter>
+  </React.StrictMode>
 );
