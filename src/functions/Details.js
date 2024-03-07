9c741462058ce533af1bc5fbf7b21bc53445213c
Iain <iain.hoolahan@gmail.com>
2024-03-07 17:09:05 +0000

    changed import syntax of details

--- src/functions/Details.js
+++ src/functions/Details.js
@@ -1,4 +1,5 @@
-async function details(place_id) {
+// details.js
+export async function details(place_id) {
   const resp = await fetch(`https://t5jalxqqsb.execute-api.eu-west-2.amazonaws.com/get-details`, {
     method: "post",
     headers: {
@@ -12,5 +13,3 @@ async function details(place_id) {
   const data = await resp.json();
   return data.data;
 }
-
-export default details;
