be248f1064fe134ed9ceec00084cc72ff85e8b46
Iain <iain.hoolahan@gmail.com>
2025-05-24 00:15:48 +0100

    smaller box for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,51 +1,66 @@
-// components/Header.js
-import { Box, Center, HStack, VStack, IconButton, Image, Heading, useBreakpointValue } from "@chakra-ui/react";
+// src/components/Header.js
+
+import {
+  Box,
+  Flex,
+  IconButton,
+  Image,
+  Heading,
+  useBreakpointValue,
+  HStack,
+  Tooltip,
+} from "@chakra-ui/react";
 import { FaLocationArrow } from "react-icons/fa";
 import tipsyTouristLogo3 from "../assets/images/logo3.svg";
-import PlanTripButtons from './plan/PlanTripButtons';
+import PlanTripButtons from "./plan/PlanTripButtons";
 
 const Header = ({ onCenter, onPlanTrip, onSeeItinerary }) => {
   const isMobile = useBreakpointValue({ base: true, md: false });
 
   return (
-    <Box bg="white" py={2} width="100%" zIndex={1}>
-      <Center>
-        {isMobile ? (
-          <VStack spacing={2} alignItems="center">
-            <HStack spacing={2} alignItems="center">
-              <Image boxSize="40px" objectFit="cover" src={tipsyTouristLogo3} alt="logo" zIndex={1} />
-              <Heading color="#393f49" size="md">Tipsy Tourist</Heading>
-              <IconButton
-                bgColor="white"
-                aria-label="center back"
-                icon={<FaLocationArrow />}
-                isRound
-                onClick={onCenter}
-              />
-            </HStack>
-            <HStack spacing={2}>
-              <PlanTripButtons onPlanTrip={onPlanTrip} onSeeItinerary={onSeeItinerary} position="left" />
-              <PlanTripButtons onPlanTrip={onPlanTrip} onSeeItinerary={onSeeItinerary} position="right" />
-            </HStack>
-          </VStack>
-        ) : (
-          <HStack spacing={2} alignItems="center">
-            <PlanTripButtons onPlanTrip={onPlanTrip} onSeeItinerary={onSeeItinerary} position="left" />
-            <Image boxSize="40px" objectFit="cover" src={tipsyTouristLogo3} alt="logo" zIndex={1} />
-            <Heading color="#393f49">Tipsy Tourist</Heading>
+    <Box
+      as="header"
+      position="sticky"
+      top={0}
+      zIndex="999"
+      bg="whiteAlpha.900"
+      backdropFilter="blur(6px)"
+      boxShadow="sm"
+      px={4}
+      py={2}
+    >
+      <Flex
+        direction={{ base: "column", md: "row" }}
+        align="center"
+        justify="space-between"
+        gap={3}
+      >
+        <HStack spacing={3} align="center">
+          <Image src={tipsyTouristLogo3} alt="logo" boxSize="50px" />
+          <Heading size={isMobile ? "md" : "lg"} color="#393f49">
+            Tipsy Tourist
+          </Heading>
+        </HStack>
+
+        <HStack spacing={3}>
+          <PlanTripButtons onPlanTrip={onPlanTrip} onSeeItinerary={onSeeItinerary} position="left" />
+          <PlanTripButtons onPlanTrip={onPlanTrip} onSeeItinerary={onSeeItinerary} position="right" />
+          <Tooltip label="Re-center map">
             <IconButton
-              bgColor="white"
-              aria-label="center back"
               icon={<FaLocationArrow />}
+              aria-label="Re-center map"
+              size="md"
               isRound
+              bg="white"
+              _hover={{ bg: "gray.100" }}
               onClick={onCenter}
+              boxShadow="sm"
             />
-            <PlanTripButtons onPlanTrip={onPlanTrip} onSeeItinerary={onSeeItinerary} position="right" />
-          </HStack>
-        )}
-      </Center>
+          </Tooltip>
+        </HStack>
+      </Flex>
     </Box>
   );
-}
+};
 
 export default Header;
